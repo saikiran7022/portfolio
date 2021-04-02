aa50f729b68dd3eb86046894a3a01fb7efa19277
saikiran7022 <saikiranlingadally@gmail.com>
2021-04-02 14:47:15 +0530

    entire portfolio neww

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
-import SanityClient from '@sanity/client'
+import sanityClient from '@sanity/client'
 
-export default SanityClient({
+export default sanityClient({
 
     "projectId": "h5j8yjeo",
     "dataset": "production"
